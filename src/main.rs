defd5f44f1aa0a4e59ed80d366b12ba27afc6993
Matt Helm <mthelm85@gmail.com>
2021-02-08 15:33:50 -0600

    write project/manifest .toml files to correct dir

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,12 @@ fn main() -> std::io::Result<()> {
 
     fs::write(Path::new(&args.name).join("src").join(format!("{}.jl", &args.name)), "println(\"Hello, world!\")")?;
 
-    fs::write("Manifest.toml", "# This file is machine-generated - editing it directly is not advised")?;
+    fs::write(Path::new(&args.name).join("Manifest.toml"), "# This file is machine-generated - editing it directly is not advised")?;
 
     let project_name = format!("name = \"{}\"\n", &args.name);
     let project_uuid = format!("uuid = \"{}\"", Uuid::new_v4());
 
-    let mut project_toml = File::create("Project.toml")?;
+    let mut project_toml = File::create(Path::new(&args.name).join("Project.toml"))?;
 
     project_toml.write_all(project_name.as_bytes())?;
     project_toml.write_all(project_uuid.as_bytes())?;
